0bcf77542761ba1ef0ab69533891e77f8ea0c26f
황순환 <hwangsunhwan@Harry.local>
2020-10-29 14:33:50 +0900

    completed bee

--- src/pseudoclassical/HoneyMakerBee.js
+++ src/pseudoclassical/HoneyMakerBee.js
@@ -1,7 +1,18 @@
 var Bee = require('./Bee');
 
 var HoneyMakerBee = function () {
-
+    Bee.call(this)
+    this.age = 10;
+    this.job = 'make honey'
+    this.honeyPot = 0;
 };
+HoneyMakerBee.prototype =Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function(){
+    this.honeyPot ++
+}
+HoneyMakerBee.prototype.giveHoney = function(){
+    this.honeyPot --
+}
 
 module.exports = HoneyMakerBee;
